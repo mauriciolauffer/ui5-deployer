70ec0f4966f6bc88b37a622720851f4e6a63a7b3
Mauricio Lauffer <lauffer@outlook.com>
2021-01-06 09:12:52 +1100

    feat: CLI is part of the pkg now

---
+++ bin/ui5-deployer.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// The following block should be compatible to as many Node.js versions as possible
+/* eslint-disable no-var */
+var pkg = require('../package.json');
+var semver = require('semver');
+var nodeVersion = process.version;
+/* eslint-enable no-var */
+if (pkg.engines && pkg.engines.node && !semver.satisfies(nodeVersion, pkg.engines.node)) {
+  console.log('==================== UNSUPPORTED NODE.JS VERSION ====================');
+  console.log('You are using an unsupported version of Node.js');
+  console.log('Detected version ' + nodeVersion + ' but ' + pkg.name + ' requires ' + pkg.engines.node);
+  console.log('');
+  console.log('=> Please upgrade to a supported version of Node.js to use this tool');
+  console.log('=====================================================================');
+  process.exit(1);
+}
+
+// Timeout is required to log info when importing from local installation
+setTimeout(() => {
+  if (!process.env.UI5_CLI_NO_LOCAL) {
+    const importLocal = require('import-local');
+    // Prefer a local installation of @ui5/cli.
+    // This will invoke the local CLI, so no further action required
+    if (importLocal(__filename)) {
+      if (process.argv.includes('--verbose')) {
+        console.info(`INFO: This project contains an individual ${pkg.name} installation which ` +
+					'will be used over the global one.');
+        console.info('');
+      } else {
+        console.info(`INFO: Using local ${pkg.name} installation`);
+        console.info('');
+      }
+      return;
+    }
+  }
+
+  const cli = require('yargs');
+
+  // Explicitly set CLI version as the yargs default might
+  // be wrong in case a local CLI installation is used
+  // Also add CLI location
+  const version = `${pkg.version} (from ${__filename})`;
+  require('../lib/cli/cli/version').set(version);
+  cli.version(version);
+
+  // Explicitly set script name to prevent windows from displaying "ui5.js"
+  cli.scriptName('ui5-deployer');
+
+  // CLI modules
+  cli.commandDir('../lib/cli/cli/commands');
+
+  // Format terminal output to full available width
+  cli.wrap(cli.terminalWidth());
+
+  // yargs registers a get method on the argv property.
+  // The property needs to be accessed to initialize everything.
+  cli.argv;
+}, 0);
---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * @module @ui5/cli
+ * @private
+ */
+module.exports = {
+  init: require('./lib/cli/init/init').init,
+};
